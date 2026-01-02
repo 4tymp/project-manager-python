97d9140dd3bfb138910ae9d8016358e9123aa884
tmp <tymoteuszpieczka@gmail.com>
2026-01-02 16:14:18 +0100

    feat: listing projects

---
+++ corefunctions/new_project.py
@@ -0,0 +1,9 @@
+import corefunctions.mainmenu as mainmenu
+from utils.clear import clear_terminal
+
+def add_new_project(projects_list):
+    clear_terminal()
+    
+    new_project_name = input(f"jak ma sie nazywac?\n")
+    projects_list.append(new_project_name)
+    print(f"dodano {new_project_name}.\n")
--- corefunctions/projects_list.py
+++ corefunctions/projects_list.py
@@ -1,10 +1,21 @@
+from corefunctions.new_project import add_new_project
+
+projects_list = ["utworz nowy"]
 
 def list_projects():
     
     project_number = None
 
-    while project_number == None:
+    while True:
         print(f"Wybierz projekt:\n")
-        print(f"0 - utworz nowy")
 
-        project_number = input()
+        indexes = 0
+        for i in projects_list:
+            print(f"{indexes} - {i}")
+            indexes += 1
+
+        project_number = input("")
+
+        if project_number == "0":
+            add_new_project(projects_list)
+    
