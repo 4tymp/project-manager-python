508284ba5c3d2ef8f5f93a6313ab411a9778245d
tmp <tymoteuszpieczka@gmail.com>
2026-01-02 15:42:50 +0100

    started working on projects list

--- corefunctions/mainmenu.py
+++ corefunctions/mainmenu.py
@@ -1,10 +1,12 @@
 from utils.logo import print_logo
 from utils.clear import clear_terminal
+from corefunctions.projects_list import list_projects
 
 
 def main():
     clear_terminal()
     print_logo()
 
-    print
     print(f"Wybierz projekt:")
+    
+    list_projects()
---
+++ corefunctions/projects_list.py
@@ -0,0 +1,3 @@
+
+def list_projects():
+    pass
