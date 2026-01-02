a794ff5b17fff952af3c0e1fe33904191b44762a
tmp <tymoteuszpieczka@gmail.com>
2026-01-02 16:19:57 +0100

    feat: finished project listing, added quitting the program function

--- corefunctions/new_project.py
+++ corefunctions/new_project.py
@@ -3,7 +3,9 @@ from utils.clear import clear_terminal
 
 def add_new_project(projects_list):
     clear_terminal()
-    
+
     new_project_name = input(f"jak ma sie nazywac?\n")
     projects_list.append(new_project_name)
+
+    clear_terminal()
     print(f"dodano {new_project_name}.\n")
--- corefunctions/projects_list.py
+++ corefunctions/projects_list.py
@@ -1,21 +1,24 @@
 from corefunctions.new_project import add_new_project
 
-projects_list = ["utworz nowy"]
+projects_list = []
 
 def list_projects():
     
     project_number = None
 
     while True:
-        print(f"Wybierz projekt:\n")
-
+        print(f"Wybierz projekt:")
+        print("add - tworzy nowy projekt | del - usuwa wybrany projekt | quit - konczy program\n")
         indexes = 0
         for i in projects_list:
             print(f"{indexes} - {i}")
             indexes += 1
 
-        project_number = input("")
+        project_number = input()
 
-        if project_number == "0":
+        if project_number == "add":
             add_new_project(projects_list)
+        
+        if project_number == "quit":
+            return False
     
